99e8a1a89ac262dbad2d1256cda73955db94bbf1
luccaki <lucca.ianaguivara@gmail.com>
2022-04-26 16:29:22 -0300

    fix

--- application.py
+++ application.py
@@ -5,9 +5,6 @@ from discord.ext import commands
 import discord
 import random
 import pafy
-from flask import Flask
-
-application = Flask(__name__)
 
 bot = commands.Bot(command_prefix='')
         
@@ -103,8 +100,5 @@ async def on_message(message):
 
   #if message.author.id == 332525747749257216:
   #  await message.channel.send("**PRIMATA!**")
-
-if __name__ == "__main__":
-    application.run(host='localhost', debug=True)
      
 bot.run(os.environ.get('TOKEN'))
